41dcbf00789406c0b660970a385791854e52c2f1
william quimbay <quimbayfeo@gmail.com>
2025-03-03 14:32:33 -0500

    index-css

--- js/script.js
+++ js/script.js
@@ -37,17 +37,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const rol = localStorage.getItem("rol");
   const authButton = document.getElementById("auth-button");
   const registerButton = document.getElementById("register-button");
+  const userAvatar = document.getElementById("user-avatar");
 
   if (userName) {
-    document.getElementById(
-      "user-name"
-    ).textContent = `Bienvenido, ${userName}`;
-    authButton.textContent = "Cerrar Sesión";
+    document.getElementById("user-name").textContent = userName;
+    authButton.innerHTML = '<i class="fas fa-sign-out-alt"></i> Cerrar Sesión';
     authButton.onclick = handleLogout;
     registerButton.style.display = "none"; // Ocultar el botón de registro
+    userAvatar.style.display = "inline-block"; // Mostrar el avatar
   } else {
-    authButton.textContent = "Iniciar Sesión";
+    authButton.innerHTML = '<i class="fas fa-sign-in-alt"></i> Iniciar Sesión';
     authButton.onclick = () => (window.location.href = "./vista/login.html");
+    userAvatar.style.display = "none"; // Ocultar el avatar
   }
 
   if (rol === "admin") {
@@ -59,7 +60,7 @@ function handleLogout() {
   localStorage.removeItem("correo");
   localStorage.removeItem("rol");
   localStorage.removeItem("nombre_usuario");
-  window.location.href = "index.html";
+  window.location.href = "./vista/login.html";
 }
 
 function handleAuthButtonClick() {
@@ -67,6 +68,6 @@ function handleAuthButtonClick() {
   if (userName) {
     handleLogout();
   } else {
-    window.location.href = "index.html";
+    window.location.href = "./vista/login.html";
   }
 }
