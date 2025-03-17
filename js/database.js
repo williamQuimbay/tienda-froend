2cfc5b48f84ea1f858bb87d8702d43ec68947539
william quimbay <quimbayfeo@gmail.com>
2025-03-16 20:10:41 -0500

    segunda entrega

--- js/database.js
+++ js/database.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "odin",
   password: "D4nt30217",
-  database: "tiendaelectronica",
+  database: "tienda",
 });
 
 connection.connect((err) => {
